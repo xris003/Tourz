b214b98ed35ec76511593d2feb2cecabe9d9a3c0
xris003 <chrsikariagabriel@gmail.com>
2023-09-20 12:03:39 +0100

    controllers

--- controllers/userController.js
+++ controllers/userController.js
@@ -33,11 +33,19 @@ const upload = multer({
 
 exports.uploadUserPhoto = upload.single('photo');
 
-exports.resizeUserPhoto = (req, res, next) => {
+exports.resizeUserPhoto = catchAsync(async (req, res, next) => {
   if (!req.file) return next();
 
-  sharp(req.file.buffer).resize(500, 500);
-};
+  req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
+
+  await sharp(req.file.buffer)
+    .resize(500, 500)
+    .toFormat('jpeg')
+    .jpeg({ quality: 90 })
+    .toFile(`public/img/users/${req.file.filename}`);
+
+  next();
+});
 
 const filterObj = (obj, ...allowedFields) => {
   const newObj = {};
