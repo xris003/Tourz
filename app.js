2367e89da65ae3555e3416b492853ce3c9bdd084
Chris-Ikaria Gabriel <chrisikariagabriel@gmail.com>
2023-09-29 15:03:21 +0100

    booking

--- app.js
+++ app.js
@@ -7,12 +7,15 @@ const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const bookingRouter = require('./routes/bookingRoutes');
 const viewRouter = require('./routes/viewsRoutes');
 
 const app = express();
@@ -22,11 +25,14 @@ app.set('views', path.join(__dirname, 'views'));
 
 // 1) GLOBAL MIDDLEWARES
 
+// Enable CORS for all routes
+//app.use(cors());
+
 // Serving Static Files
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Security HTTP headers
-app.use(helmet());
+app.use(helmet({ contentSecurityPolicy: false }));
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
@@ -43,6 +49,8 @@ app.use('/api', limiter);
 
 //Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+app.use(cookieParser());
 
 // Data Sanitization against NoSQL query injection
 app.use(mongoSanitize());
@@ -57,12 +65,20 @@ app.use(
       'duration',
       'ratingsQuantity',
       'ratingsAverage',
-      'maxGroup size',
+      'maxGroupSize',
       'difficulty',
       'price',
     ],
   })
 );
+
+app.use((req, res, next) => {
+  res.setHeader(
+    'Content-Security-Policy-Report-Only',
+    "script-src 'self' https://api.mapbox.com https://js.stripe.com/v3/; worker-src 'self' blob:"
+  );
+  next();
+});
 
 // app.use((req,res,next) => {
 //   console.log('Hello from the middleware');
@@ -72,7 +88,7 @@ app.use(
 // Test Middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
-  console.log(req.headers);
+  //console.log(req.cookies);
   next();
 });
 
@@ -87,6 +103,7 @@ app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
+//app.use('/api/v1/bookings', bookingRouter);
 
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
